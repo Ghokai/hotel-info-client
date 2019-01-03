5c956b80e2bbdf725096f16ff1d7df5f737a97ec
Gokay Arpaci <gokayarpaci@gmail.com>
2019-01-03 18:37:12 +0300

    ok

--- src/state/store/index.js
+++ src/state/store/index.js
@@ -7,7 +7,9 @@ const store = createStore(
   {},
   compose(
     applyMiddleware(thunk),
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+    window.__REDUX_DEVTOOLS_EXTENSION__
+      ? window.__REDUX_DEVTOOLS_EXTENSION__()
+      : f => f
   )
 );
 export default store;
